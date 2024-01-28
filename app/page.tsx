5d57c05c07371f618a6148a8dcc61c4dcd1b2abe
Angel Fabian HC <angelfabianhc@gmail.com>
2024-01-28 18:06:20 -0500

    Dashboard ACME Fixe

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <h1 className="text-blue-500">I'm blue!</h1>
+          <h1 className="text-blue-500">Im blue!</h1>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
